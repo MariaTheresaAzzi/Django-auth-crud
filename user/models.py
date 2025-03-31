04f4a1b02cf2e2b2b56b796a3583dcabbe42a18a
Mariaazzi <mariatheresaelkazzi@gmail.com>
2025-03-31 16:13:17 +0300

    added all the changes for auth

--- user/models.py
+++ user/models.py
@@ -2,24 +2,24 @@ from django.db import models
 
 # Create your models here.
 
-class Doctor(models.Model):
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
-    specialization = models.CharField(max_length=255)
-    department = models.CharField(max_length=255)
+# class Doctor(models.Model):
+#     firstname = models.CharField(max_length=255)
+#     lastname = models.CharField(max_length=255)
+#     specialization = models.CharField(max_length=255)
+#     department = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.name  # Show doctor's name in admin panel & queries
+#     def __str__(self):
+#         return self.name  # Show doctor's name in admin panel & queries
 
 
-class Patient(models.Model):
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
-    address = models.TextField()
-    date_of_birth = models.DateField()
-    medical_file = models.FileField(upload_to='medical_files/', blank=True, null=True)
-    image = models.ImageField(upload_to='patient_images/', blank=True, null=True)
-    doctor = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True, related_name="patients")
+# class Patient(models.Model):
+#     firstname = models.CharField(max_length=255)
+#     lastname = models.CharField(max_length=255)
+#     address = models.TextField()
+#     date_of_birth = models.DateField()
+#     medical_file = models.FileField(upload_to='medical_files/', blank=True, null=True)
+#     image = models.ImageField(upload_to='patient_images/', blank=True, null=True)
+#     doctor = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True, related_name="patients")
 
-    def __str__(self):
-        return self.name  # Show patient's name in admin panel & queries
+#     def __str__(self):
+#         return self.name  # Show patient's name in admin panel & queries
--- user/views.py
+++ user/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-
+@login_required
 # Create your views here.
 def home(request):
     # return HttpResponse("Hello, world. You're at the user home page.")
